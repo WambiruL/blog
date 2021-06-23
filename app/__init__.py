4c1b7b542aa59185b5252162e5a5e196a29c14f3
WambiruL <wambirulorraine@gmail.com>
2021-06-23 16:46:32 +0300

    Add user login page, and welcome email

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,24 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
+from flask_mail import Mail
 import os
 
 bootstrap=Bootstrap()
 db=SQLAlchemy()
+mail = Mail()
+
 login_manager =LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = "auth.login"
 #initializing app
 def create_app(config_name):
     app=Flask(__name__)
+     
 
 # Initializing Flask Extensions
     bootstrap = Bootstrap(app)
+    mail.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
 
@@ -27,5 +32,8 @@ def create_app(config_name):
  # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
 
     return app
--- config.py
+++ config.py
@@ -1,8 +1,16 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
     SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://lorraine:1234@localhost/blog'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
