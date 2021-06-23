f804b86a7d261c5fc50c2f5ddacef241030b8ae8
WambiruL <wambirulorraine@gmail.com>
2021-06-23 09:47:27 +0300

    Add the User db model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from. import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Post(db.Model):
     __tablename__ = "posts"
@@ -25,3 +27,33 @@ class Post(db.Model):
     @classmethod
     def get_all_posts(cls):
         return Post.query.order_by(Post.posted_at).all()
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique = True)
+    email = db.Column(db.String(255), unique = True, index = True)
+    bio = db.Column(db.String())
+    avatar_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    posts = db.relationship("Post", backref = "user",lazy = "dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError("You cannot read the password attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+   
+    def __repr__(self):
+        return f"User {self.username}"
+
