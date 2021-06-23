8c5d84daa9a0e9178062151e309773cdbe64dd03
WambiruL <wambirulorraine@gmail.com>
2021-06-23 17:56:59 +0300

    Migration

---
+++ migrations/versions/3c21d652698f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3c21d652698f
+Revises: 
+Create Date: 2021-06-23 17:56:02.452985
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3c21d652698f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
